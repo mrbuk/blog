09390e657ed264e5b85b1e17950bc0ff5174623b
Markus Bukowski <me@mrbuk.de>
2017-07-03 22:39:04 +0200

    Adapted build.sh based on https://gohugo.io/tutorials/github-pages-blog/

--- build.sh
+++ build.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
-# remove current work dir
-rm -rf ./public || exit 1
+# cleanup
+rm -rf public || exit 1
+mkdir public
+git worktree prune || exit 1
+rm -rf .git/worktrees/public/
 
 # checkout gh-pages to public dir
-git worktree add public origin/gh-pages || exit 1
+git worktree add -b gh-pages public origin/gh-pages || exit 1
+
+rm -rf public/*
 
 # build, will create new 
-hugo || exit 1
+hugo -t slim || exit 1
 
 last_commit_msg=$(git log --pretty=oneline | head -n1 | perl -pe 's|\w+\s+(.+)\s*$|\1|')
 echo "\nPlease check the code and run something like:"
 cat << EOF
-cd ./public &&\\
- git add --all &&\\
- git commit -m '$last_commit_msg' &&\\
- git push origin/gh-pages &&\\
+cd ./public &&
+ git add --all &&
+ git commit -m '$last_commit_msg' &&
+ git push &&
  popd
 EOF
