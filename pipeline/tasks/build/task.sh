bb112be08b26520ad41d87c04245d1f9da532e01
Markus Bukowski <me@mrbuk.de>
2017-11-08 00:59:34 +0100

    Added gh-pages as input for build task

--- pipeline/tasks/build/task.sh
+++ pipeline/tasks/build/task.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+cd gh-pages
+git rm -rf .
+cd ..
 
+# create new version
 cd blog
-rm -rf public/*
-hugo -t slim
-echo "mrbuk.de" > public/CNAME
+hugo -t slim -d ../new-gh-pages
+cd ..
+
+echo "mrbuk.de" > new-gh-pages/CNAME
+cp -R gh-pages/.git* new-gh-pages/
+
+cd new-gh-pages
+git config --global user.email "co-pilot@doesnt.exist"
+git config --global user.name "Co-Pilot"
+
+git add .
+git commit -m 'autobuild'
